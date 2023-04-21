999419546411668f8bcb82334f3f7e35926635fe
Felipe Versiane <felipeversiane09@gmail.com>
2023-04-21 09:06:12 -0300

    :pencil:

--- app/views.py
+++ app/views.py
@@ -6,3 +6,11 @@ def home(request):
     return render(request, 'app/home.html')
 
 
+def login(request):
+    return render(request, 'app/login.html')
+
+
+def create_acount(request):
+    return render(request, 'app/create_acount.html')
+
+
--- server/urls.py
+++ server/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from app.views import home, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',    include('app.urls')),
-    path('app/', include('app.urls')),
+    path('',  home),
+    path("login/", login),
 ]
